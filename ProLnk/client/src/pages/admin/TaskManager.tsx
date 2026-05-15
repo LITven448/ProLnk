0e23dc29c10932b57d9d6f13bf19c824eb13b56e
LITven448 <andrew@lit-ventures.com>
2026-05-15 17:30:15 -0500

    fix: remove import type React from TaskManager.tsx

--- ProLnk/client/src/pages/admin/TaskManager.tsx
+++ ProLnk/client/src/pages/admin/TaskManager.tsx
@@ -1,4 +1,3 @@
-import type React from "react";
 import { useState, useRef } from "react";
 import { Link } from "wouter";
 import AdminLayout, { T, BADGE_GRADIENTS, FONT } from "@/components/AdminLayout";

00af4eb989d121b12707ba5722dfa91d5cc4a3c8
LITven448 <andrew@lit-ventures.com>
2026-05-15 17:57:01 -0500

    fix: React import in FranchiseTerritories.tsx

--- ProLnk/client/src/pages/admin/FranchiseTerritories.tsx
+++ ProLnk/client/src/pages/admin/FranchiseTerritories.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import type React from "react";
 import { useState } from "react";
 import AdminLayout, { T, BADGE_GRADIENTS, FONT, MONO } from "@/components/AdminLayout";

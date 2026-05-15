7c8221fa637b0a612316914714ff4ea1b6d36590
LITven448 <andrew@lit-ventures.com>
2026-05-15 18:30:45 -0500

    revert: PartnerLayout.tsx to last-known-good state

--- ProLnk/client/src/components/PartnerLayout.tsx
+++ ProLnk/client/src/components/PartnerLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import type React from "react";
 import { Link, useLocation } from "wouter";
 import { useAuth } from "@/_core/hooks/useAuth";
 import ProLnkLogo from "@/components/ProLnkLogo";

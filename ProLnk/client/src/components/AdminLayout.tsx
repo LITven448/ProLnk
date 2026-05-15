b6611f54420f2168e1cffb068c72aef4602ccd05
LITven448 <andrew@lit-ventures.com>
2026-05-15 18:30:28 -0500

    revert: AdminLayout.tsx to last-known-good state

--- ProLnk/client/src/components/AdminLayout.tsx
+++ ProLnk/client/src/components/AdminLayout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import type React from "react";
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "wouter";
 import ErrorBoundary from "@/components/ErrorBoundary";
